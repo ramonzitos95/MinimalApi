1eec1954bed77c4130a8749e354c0faacee038c3
Ramon Silva <ramonss.bque@gmail.com>
2023-06-08 20:03:52 -0300

    App minimal API

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapGet("/", () => "Seja bem vindo ao sistema");
+
+app.MapGet("/posts", () => {
+    return "Posts";
+});
+
+app.Run();
